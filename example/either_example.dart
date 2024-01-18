8a7e5b78d7a30f5eada3bf7674c42aef10040150
Felipe Duarte Silva <feliped.dev.contato@gmail.com>
2024-01-18 06:45:00 -0300

    feat(core): :sparkles: implements core package feature
    Signed-off-by: Felipe Duarte Silva <feliped.dev.contato@gmail.com>

--- example/either_example.dart
+++ example/either_example.dart
@@ -1,6 +1,41 @@
 import 'package:either/either.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final foo = Foo();
+  final result1 = foo.execute('42');
+
+  result1.fold(
+    (l) => print(l.exception),
+    (r) => print(r.value), // 42
+  );
+
+  final result2 = foo.execute('foo');
+
+  result2.fold(
+    (l) => print(l.exception), // FormatException
+    (r) => print(r.value),
+  );
+}
+
+class Foo {
+  Either<Baz, Bar> execute(dynamic params) {
+    try {
+      final result = int.parse(params);
+      return Right(Bar(result));
+    } on Exception catch (e) {
+      return Left(Baz(e));
+    }
+  }
+}
+
+class Bar {
+  final int value;
+
+  const Bar(this.value);
+}
+
+class Baz {
+  final Exception exception;
+
+  const Baz(this.exception);
 }
--- lib/src/either_base.dart
+++ lib/src/either_base.dart
@@ -1,6 +1,70 @@
-// TODO: Put public facing types in this file.
+abstract class Either<L, R> {
+  B fold<B>(B Function(L l) ifLeft, B Function(R r) ifRight);
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+  bool isLeft();
+  bool isRight();
+
+  Left<L, R> getLeft();
+  Right<L, R> getRight();
+
+  L getLeftValue();
+  R getRightValue();
 }
+
+class Left<L, R> extends Either<L, R> {
+  final L left;
+
+  Left(this.left);
+
+  @override
+  B fold<B>(B Function(L l) ifLeft, B Function(R r) ifRight) => ifLeft(left);
+
+  @override
+  bool isLeft() => true;
+
+  @override
+  bool isRight() => false;
+
+  @override
+  Left<L, R> getLeft() => this;
+
+  @override
+  Right<L, R> getRight() => throw NotRightException();
+
+  @override
+  L getLeftValue() => left;
+
+  @override
+  R getRightValue() => throw NotRightException();
+}
+
+class Right<L, R> extends Either<L, R> {
+  final R right;
+
+  Right(this.right);
+
+  @override
+  B fold<B>(B Function(L l) ifLeft, B Function(R r) ifRight) => ifRight(right);
+
+  @override
+  bool isLeft() => false;
+
+  @override
+  bool isRight() => true;
+
+  @override
+  Left<L, R> getLeft() => throw NotLeftException();
+
+  @override
+  Right<L, R> getRight() => this;
+
+  @override
+  L getLeftValue() => throw NotLeftException();
+
+  @override
+  R getRightValue() => right;
+}
+
+class NotRightException implements Exception {}
+
+class NotLeftException implements Exception {}
